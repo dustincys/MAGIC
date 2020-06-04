3f65ec772c41e95b47112e55f9a434414d03b292
dustin <yanshuochu@qq.com>
2020-06-03 21:41:24 -0500

    remove modification

--- python/magic/magic.py
+++ python/magic/magic.py
@@ -827,7 +827,7 @@ class MAGIC(BaseEstimator):
                         error_vec.append(error)
                         _logger.debug("{}: {}".format(i, error_vec))
                         if error < threshold and t_opt is None:
-                            t_opt = i
+                            t_opt = i + 1
                             _logger.info("Automatically selected t = {}".format(t_opt))
 
         if plot:
